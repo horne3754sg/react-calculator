e045c9f88ec116b0dd8bb58475cc31a277f687d2
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-05-21 17:39:42 +0100

    initial text scaler functional component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { TextScaler } from '../utils/TextScaler'
 import CalculatorKeypad from './CalculatorKeypad'
 
 const Calculator = (props) => {
   return (
     <div className='calculator'>
-      <div className='calculator-display'>{props.displayValue}</div>
+      <div className='calculator-display'>
+        <TextScaler>{props.displayValue}</TextScaler>
+      </div>
       <CalculatorKeypad {...props} />
     </div>
   )
---
+++ src/utils/TextScaler.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const TextScaler = (props) => {
+  return <span>{props.children}</span>
+}
