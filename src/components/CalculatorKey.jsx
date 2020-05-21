c8e0a54e4939abb0183aa9c5f6ad9d9cf011587e
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-05-21 15:43:57 +0100

    key is passed directly through the function callback

--- src/components/CalculatorKey.jsx
+++ src/components/CalculatorKey.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 const CalculatorKey = (props) => {
-  const { label, charCode, classes, onClick, keyPress } = props
+  const { label, charCode, classes, onClick } = props
   let classNames = classes ? `calculator-keys ${classes}` : 'calculator-keys'
   let text = charCode ? String.fromCharCode(charCode) : label
 
   return (
-    <button
-      className={classNames}
-      onClick={onClick ? () => onClick(keyPress || '') : null}>
+    <button className={classNames} onClick={onClick ? onClick : null}>
       {text}
     </button>
   )
